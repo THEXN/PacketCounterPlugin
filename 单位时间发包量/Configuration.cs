760de0e1e12ae3aae3ba577595cf65c9b87de424
xien <2383759126@qq.com>
2024-03-09 17:44:06 +0800

    添加项目文件。

---
+++ 单位时间发包量/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace PacketCounterPlugin
+{
+    public class Configuration
+    {
+        public static readonly string FilePath = Path.Combine(TShock.SavePath, "单位时间发包量.json");
+
+        public double 向控制台报告时间 = 60.0; // 转换为秒
+
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(str);
+                }
+            }
+        }
+
+        public static Configuration Read(string path)
+        {
+            if (!File.Exists(path))
+                return new Configuration();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var cf = JsonConvert.DeserializeObject<Configuration>(sr.ReadToEnd());
+                    return cf;
+                }
+            }
+        }
+    }
+}
---
+++ 单位时间发包量/单位时间发包量.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace PacketCounterPlugin
+{
+    [ApiVersion(2, 1)]
+    public class PacketCounterPlugin : TerrariaPlugin
+    {
+        private readonly Dictionary<int, PacketStats> playerStats = new Dictionary<int, PacketStats>();
+        public static Configuration Config;
+        public PacketCounterPlugin(Main game) : base(game)
+        {
+            LoadConfig();
+        }
+        private static void LoadConfig()
+        {
+            Config = Configuration.Read(Configuration.FilePath);
+            Config.Write(Configuration.FilePath);
+
+        }
+        private static void ReloadConfig(ReloadEventArgs args)
+        {
+            LoadConfig();
+            args.Player?.SendSuccessMessage("[{0}] 重新加载配置完毕。", typeof(PacketCounterPlugin).Name);
+        }
+        public override void Initialize()
+        {
+            GeneralHooks.ReloadEvent += ReloadConfig;
+            ServerApi.Hooks.NetSendData.Register(this, OnSendData);
+            ServerApi.Hooks.GameUpdate.Register(this, OnGameUpdate);
+        }
+
+        private void OnSendData(SendDataEventArgs args)
+        {
+            if (args.remoteClient < 0 || args.remoteClient >= 255)
+                return;
+
+            int playerIndex = args.remoteClient;
+
+            if (!playerStats.TryGetValue(playerIndex, out PacketStats stats))
+            {
+                stats = new PacketStats();
+                playerStats[playerIndex] = stats;
+            }
+
+            stats.AddPacket();
+        }
+
+        private void OnGameUpdate(EventArgs args)
+        {
+            double elapsedSeconds = Config.向控制台报告时间;
+
+            foreach (var kvp in new Dictionary<int, PacketStats>(playerStats))
+            {
+                int playerIndex = kvp.Key;
+                PacketStats stats = kvp.Value;
+
+                if (stats.ElapsedSeconds >= elapsedSeconds)
+                {
+                    LogPacketInfo(playerIndex, stats.PacketCount);
+                    stats.Reset();
+                }
+            }
+        }
+
+        private void LogPacketInfo(int playerIndex, int packetCount)
+        {
+            TSPlayer tsplayer = TShock.Players[playerIndex];
+            string playerName = tsplayer?.Name ?? "Unknown";
+
+            string logMessage = $"玩家 {playerName} 在过去 {Config.向控制台报告时间} 秒内发送了 {packetCount} 个数据包。";
+            TShock.Log.ConsoleInfo(logMessage);
+        }
+
+        private class PacketStats
+        {
+            private int packetCount;
+            private DateTime lastResetTime;
+
+            public int PacketCount => packetCount;
+            public double ElapsedSeconds => (DateTime.Now - lastResetTime).TotalSeconds;
+
+            public void AddPacket()
+            {
+                packetCount++;
+            }
+
+            public void Reset()
+            {
+                packetCount = 0;
+                lastResetTime = DateTime.Now;
+            }
+        }
+    }
+}
